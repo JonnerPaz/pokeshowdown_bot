9205023e93f0deceee5ac37dd4addd7d1b72e99f
JonnerPaz <jonnerpazp@gmail.com>
2024-02-13 00:24:49 -0400

    Fix register command and working on delete command

--- src/User.ts
+++ src/User.ts
@@ -6,11 +6,13 @@ interface RegisteredUser {
 }
 
 export class User {
+  private id: number
   private user: string
   private data: RegisteredUser
 
-  constructor(user: string) {
+  constructor(user: string, id: number) {
     this.user = user
+    this.id = id
 
     // creates initial data of a given user
     this.data = {
@@ -21,6 +23,14 @@ export class User {
 
   get getData() {
     return this.data
+  }
+
+  /**
+   *
+   * returns ID
+   * @param get id */
+  get getID() {
+    return this.id
   }
 
   start(bot: Bot) {
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard } from "grammy";
 import { Pokemon } from "./pokeapi";
 import { User } from "./User";
 
 const API_KEY = "6836934004:AAHpDd_rCqfMwQOdzJWW6ljjoLDomELq5w4";
 
 const bot = new Bot(API_KEY);
+
+// Here must be stored all accounts in the group that are registered by /register
+let userDB: User[] = []
 
 bot.command("start", async ctx => {
   await bot.start();
@@ -14,21 +17,43 @@ bot.command("start", async ctx => {
 
 bot.command("register", ctx => {
   const userName = ctx.from?.username as string
+
   // Creates a new user. telegram Usernames are used as ID
-  const user = new User(userName)
-  console.log(user.getData)
-  // console.log('chatMember', ctx.chatMember) // groups maybe?
-  // const adminRights = await ctx.getMyDefaultAdministratorRights() // Later use
-  // const chat = await ctx.getChat() // Possibly use for later
+  const newUser = new User(userName, 1)
+
+  const condition = userDB.some(el => el.getData.userName === newUser.getData.userName)
+  if (condition) {
+    return ctx.reply(`You are already registered. If you want to erase your data and start over, use /deleteAccount`)
+  }
+
+  // inserts new user into userDB array
+  userDB.push(newUser)
 })
 
-bot.command("pokemonGenerate", async ctx => {
+/* bot.command("pokemonGenerate", async ctx => {
   try {
     // here goes what is used in pokemon api 
   } catch (err) {
     console.log(err)
   }
+}) */
+
+bot.command("deleteAccount", async ctx => {
+  await ctx.reply(`@${ctx.from?.username} type "yes" if you want to delete your account`)
+  const inlnKeyboard = new InlineKeyboard().text('YES', "delete").text('NO')
+  await ctx.reply('Delete your account (including all of your data)?', {
+    reply_markup: inlnKeyboard,
+  })
 })
+
+/* bot.callbackQuery("delete", async ctx => {
+  console.log("log from callbackQuery")
+  const context = <string> ctx.from?.username
+  const logic = userDB.some(el => el.getData.userName === context)
+  console.log(userDB)
+  await ctx.reply("Your account has now been erased. Sad to see you go!")
+
+}) */
 
 bot.command("stop", async ctx => {
   ctx.reply('this bot is stopping...')
@@ -36,25 +61,13 @@ bot.command("stop", async ctx => {
 })
 
 
+// commands can't have uppercase
 bot.api.setMyCommands([
   { command: "start", description: "Starts the bot" },
   { command: "register", description: "register a user into the bot" },
+  { command: "deleteaccount", description: "Delete your account from PokeBotShowdown" },
   { command: "stop", description: "Stops PokeBotShowdown" },
   // { command: "pokemonGenerate", description: "Start generating random pokemon" }
 ])
-
-// prove if bot is working
-/* bot.on("message", async res => {
-  try {
-    const message: string = res.message.text ?? "pikachu";
-    const pokemon: string = await PokeBotShowdown.pokeRenderer(message);
-
-    setInterval(() => res.api.sendPhoto(res.chat.id, pokemon), 5000);
-    return res.api.sendPhoto(res.chat.id, pokemon);
-  } catch (err) {
-    console.error(err);
-    return res.reply(`There was a problem: ${err}`);
-  }
-}); */
 
 bot.start()
