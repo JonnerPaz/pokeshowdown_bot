501dddd94f0414dbbba2e030a1a1a8c6e5cff6e0
JonnerPaz <jonnerpazp@gmail.com>
2024-02-15 00:37:05 -0400

    Add few minor logic fixes to delete account and start tinkering user.ts

--- src/User.ts
+++ src/User.ts
@@ -1,34 +1,66 @@
-import { Bot } from "grammy";
+import { Bot, CommandContext, Context } from "grammy";
+import { PokemonClient } from "pokenode-ts";
 
-interface RegisteredUser {
+export interface RegisteredUser {
   userName: string,
-  pokemon: object[],
+  data: {
+    userName: string,
+    pokemon: object[]
+  }
 }
 
-export class User {
-  private user: string
-  private data: RegisteredUser
+export class User implements RegisteredUser {
+  userName: string
+  data: {
+    userName: string,
+    pokemon: object[]
+  }
+  pokeApi: PokemonClient
 
   constructor(user: string) {
-    this.user = user
+    this.userName = user
 
     // creates initial data of a given user
     this.data = {
-      userName: this.user,
+      userName: this.userName,
       pokemon: [{}]
     }
+
+    this.pokeApi = new PokemonClient()
   }
 
-  get getData() {
+  get getUserData() {
     return this.data
   }
 
-  start(bot: Bot) {
-    const welcomeMessage = `Bienvenido a PokeBotShowdown. Bot creado para capturar, intercambiar y combatir con pokemones`;
-    bot.command("start", res => res.reply("Bienvenido a PokeBotShowDown"));
+  get getPokemonSummary() {
+    return
   }
 
-  catchPokemon() { }
+  async generatePokemon(pokemon: string, ctx?: CommandContext<Context>) {
+    /* try {
+      // receives and converts raw data from telegram api to pokeapi
+      const request = pokemon.toLowerCase();
+      const pokemonFromApi = await pokemonClient.getPokemonByName(request);
+      console.log(pokemonFromApi.sprites.front_default);
+      return pokemonFromApi.sprites.front_default;
+    } catch (err) {
+      console.error(err);
+      throw new Error(`${err}`);
+    } */
+  }
+
+  stopGeneratePokemon() {
+
+  }
+
+  generatePokemonNow() {
+
+  }
+
+  catchPokemon() {
+
+  }
 
   viewPokemon() { }
 
--- src/index.ts
+++ src/index.ts
@@ -15,58 +15,53 @@ bot.command("start", async ctx => {
 });
 
 bot.command("register", ctx => {
-  const userName = ctx.from?.username as string
-
-  // Creates a new user. telegram Usernames are used as ID
-  const newUser = new User(userName)
-
-  const condition = userDB.some(el => el.getData.userName === newUser.getData.userName)
+  const userName = <string>ctx.from?.username
+  const condition = userDB.some(el => el.getUserData.userName === userName)
   if (condition) {
     return ctx.reply(`You are already registered. If you want to erase your data and start over, use /deleteaccount`)
   }
 
-  // inserts new user into userDB array
+  // creates new user
+  const newUser = new User(userName)
   userDB.push(newUser)
   ctx.reply(`@${ctx.from?.username} have been registered`)
 })
 
-/* bot.command("pokemonGenerate", async ctx => {
+bot.command("pokemongenerate", async ctx => {
   try {
     // here goes what is used in pokemon api 
   } catch (err) {
     console.log(err)
   }
-}) */
+})
 
 bot.command("deleteaccount", async ctx => {
-  const inlnKeyboard = new InlineKeyboard().text('YES', "delete").text('NO', "nothing")
-  await ctx.reply(`@${ctx.from?.username} type "yes" if you want to delete your account`)
-  await ctx.reply('Delete your account (including all of your data)?', {
-    reply_markup: inlnKeyboard,
-  })
+  try {
+    // if not a user, return
+    const logic = userDB.some(el => el.getUserData.userName === <string>ctx.from?.username)
+    const msg = `You're not registered in @${ctx.me.username}. Use /register to use this bot`
+    if (!logic) return await ctx.reply(msg)
+
+    const inlnKeyboard = new InlineKeyboard().text('YES', "delete").text('NO', "nothing")
+    await ctx.reply(`@${ctx.from?.username} type "yes" if you want to delete your account`)
+    await ctx.reply('Delete your account (including all of your data)?', {
+      reply_markup: inlnKeyboard,
+    })
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 bot.callbackQuery("delete", async ctx => {
   // delete inline_keyboard
   ctx.deleteMessages([<number>ctx.msg?.message_id])
-  // Way to match user with userDB
-  const userName = <string>ctx.from?.username
-  const logic = userDB.some(el => el.getData.userName === userName) // true
-  console.log(ctx.msg?.message_id)
-  console.log(ctx.msg?.reply_markup?.inline_keyboard)
 
-  // if user already exists, erase it
-  if (logic) {
-    const findUser = <User>userDB.find(el => el.getData.userName === userName)
-    userDB.splice(userDB.indexOf(findUser))
-    const msg = "Your account has now been erased. Sad to see you go!"
-    await ctx.answerCallbackQuery(msg)
-    await ctx.reply(msg)
-  } else {
-    const msg = `@${ctx.from.username} You're not registered. You can't erase other's accounts!`
-    await ctx.answerCallbackQuery(msg)
-    await ctx.reply(msg)
-  }
+  const findUser = <User>userDB.find(el => el.getUserData.userName === <string>ctx.from?.username)
+  userDB.splice(userDB.indexOf(findUser))
+
+  const msg = "Your account has now been erased. Sad to see you go!"
+  await ctx.answerCallbackQuery(msg)
+  await ctx.reply(msg)
 })
 
 bot.callbackQuery("nothing", async ctx => {
@@ -79,7 +74,7 @@ bot.command("stop", async ctx => {
 })
 
 bot.command("help", async ctx => {
-  ctx.reply(`Here are some of the following commands that @${ctx.me.username} receives:
+  await ctx.reply(`Here are some of the following commands that @${ctx.me.username} receives:
 * /start - Starts de bot
 * /register - register a user into the bot
 * /deleteaccount - Delete your account from PokeBotShowdown
@@ -94,7 +89,7 @@ bot.api.setMyCommands([
   { command: "deleteaccount", description: "Delete your account from PokeBotShowdown" },
   { command: "stop", description: "Stops PokeBotShowdown" },
   { command: "help", description: "Show all commands" },
-  // { command: "pokemonGenerate", description: "Start generating random pokemon" }
+  { command: "pokemongenerate", description: "Start generating random pokemon" }
 ])
 
 bot.start()
